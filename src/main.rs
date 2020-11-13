c83aeaf3ae8f87742fb7a5fe2703968f23e814f7
Yasin Radi <yasin.ben.hamman@gmail.com>
2020-11-13 19:11:51 +0100

    Added text view to hold ascii cat.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 extern crate cursive;
 
+use cursive::views::TextView;
 
 fn main() {
     let mut siv = cursive::default();
+    let cat_text = "Meow!
+    \\  
+    \\    
+      /\\_/\\   
+     ( o o )   
+     =( I )=";
     
-    // start event loop
+    // App layout declaration
+    siv.add_layer(TextView::new(cat_text));
+
+    // Event loop start
     siv.run();
 }
