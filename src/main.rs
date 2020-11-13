3eaed8d02ea006eba6edc63356f0914414b80392
Yasin Radi <yasin.ben.hamman@gmail.com>
2020-11-13 19:37:08 +0100

    Set up window layout and app event handlers.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
-extern crate cursive;
+extern crate gio;
+extern crate gtk;
+
+use std::env;
+use gio::prelude::*;
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
 
 
 fn main() {
-    let mut siv = cursive::default();
-    
-    // start event loop
-    siv.run();
+    let app = Application::new(
+        Some("com.yasinr.catsay_gui"),
+        gio::ApplicationFlags::empty()
+    ).expect("Failed to initialize GTK.");
+
+    // Application set up
+    app.connect_startup(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("Catsay");
+        window.set_default_size(350, 70);
+
+        window.connect_delete_event(|win, _| {
+            unsafe {
+                win.destroy();
+            }
+            Inhibit(false)
+        });
+
+        window.show_all();
+    });
+
+    app.connect_activate(|_| {});
+    app.run(&env::args().collect::<Vec<_>>());
 }
