1f1fe11e01d84c91463170b1d3993ac89518df98
Yasin Radi <yasin.ben.hamman@gmail.com>
2020-11-13 19:49:08 +0100

    Added image loading and cat image.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ extern crate gtk;
 use std::env;
 use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{
+    Image,
+    Label,
+    Orientation,
+    Application, 
+    ApplicationWindow
+};
 
+const CAT_IMG_PATH: &str = "images/cat.jpg";
 
 fn main() {
     let app = Application::new(
@@ -26,6 +33,16 @@ fn main() {
             Inhibit(false)
         });
 
+        let layout_box = gtk::Box::new(Orientation::Vertical, 0);
+        let label = Label::new(Some("Meow!\n     \\\n       \\"));
+        
+        layout_box.add(&label);
+
+        let cat_image = Image::from_file(CAT_IMG_PATH);
+
+        layout_box.add(&cat_image);
+
+        window.add(&layout_box);
         window.show_all();
     });
 
