073a77f1f9fdf9c2d247936f6ce62c4823ba9258
Yasin Radi <yasin.ben.hamman@gmail.com>
2020-11-14 17:01:50 +0100

    Base glade design and handlers.

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,62 @@
 extern crate gio;
 extern crate gtk;
 
-use std::env;
+use std::env::args;
 use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{
-    Image,
-    Label,
-    Orientation,
-    Application, 
-    ApplicationWindow
-};
 
-const CAT_IMG_PATH: &str = "images/cat.jpg";
+fn build_ui(app: &gtk::Application) {
+    let glade_src = include_str!("glade_config.glade");
+    let builder = gtk::Builder::from_string(glade_src);
+
+    let window: gtk::Window = builder
+        .get_object("applicationwindow1")
+        .unwrap();
+    
+    window.set_application(Some(app));
+
+    // Inputs
+    let message_input: gtk::Entry = builder
+        .get_object("message_input")
+        .unwrap();
+    
+    // Submit button
+    let button: gtk::Button = builder
+        .get_object("generate_btn")
+        .unwrap();
+    
+    // Outputs
+    let message_output: gtk::Label = builder
+        .get_object("message_output")
+        .unwrap();
+    let image_output: gtk::Image = builder
+        .get_object("image_output")
+        .unwrap();
+    let image_output_clone = image_output.clone();
+
+    button.connect_clicked(move |_| {
+        message_output.set_text(&format!(
+            "{}\n   \\\n    \\",
+            message_input
+                .get_text()
+                .as_str()
+        ));
+        image_output_clone.show();
+    });
+
+    window.show_all();
+    image_output.hide();
+}
 
 fn main() {
-    let app = Application::new(
-        Some("com.yasinr.catsay_gui"),
+    let app = gtk::Application::new(
+        Some("com.yasinr.catsay_gui-glade"),
         gio::ApplicationFlags::empty()
     ).expect("Failed to initialize GTK.");
 
-    // Application set up
-    app.connect_startup(|app| {
-        let window = ApplicationWindow::new(app);
-        window.set_title("Catsay");
-        window.set_default_size(350, 70);
-
-        window.connect_delete_event(|win, _| {
-            unsafe {
-                win.destroy();
-            }
-            Inhibit(false)
-        });
-
-        let layout_box = gtk::Box::new(Orientation::Vertical, 0);
-        let label = Label::new(Some("Meow!\n     \\\n       \\"));
-        
-        layout_box.add(&label);
-
-        let cat_image = Image::from_file(CAT_IMG_PATH);
-
-        layout_box.add(&cat_image);
-
-        window.add(&layout_box);
-        window.show_all();
+    app.connect_activate(|app| {
+        build_ui(app);
     });
 
-    app.connect_activate(|_| {});
-    app.run(&env::args().collect::<Vec<_>>());
+    app.run(&args().collect::<Vec<_>>());
 }
